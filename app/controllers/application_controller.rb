c2ec12be07aadb0655210361f3f32ef7dd8bfbd9
= <=>
2014-04-09 14:48:39 +0300

    login fix

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,7 @@ class ApplicationController < ActionController::Base
   private
 
   def current_user
-    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+    session[:user_id].present?
   end
   def authenticate
     redirect_to login_path unless current_user or controller_name == 'sessions'
