c0df98a6aa00b1cd1503cd84d642c30d7c79b5c3
Oleg Pasko <olegpasko@gmail.com>
2014-04-14 11:10:52 +0300

    ready on page load

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -19,7 +19,8 @@
 
 
 
-$(document).ready(function(){
+var ready;
+ready = function () {
     $('form').on('click', '.remove_fields', function(event){
         $(this).prev('input[type=hidden]').val('1');
         $(this).closest('fieldset').hide();
@@ -36,3 +37,5 @@ $(document).ready(function(){
     $('.hint').tooltip();
 
 });
+$(document).ready(ready);
+$(document).on('page:load', ready);
