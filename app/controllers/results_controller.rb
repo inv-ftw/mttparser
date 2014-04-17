cda64f47340a37ed1fba418c524e26f6d0115d35
Oleksandr Poltavets <sancho.ck@gmail.com>
2014-04-17 14:02:11 +0300

    main page forms

--- app/controllers/results_controller.rb
+++ app/controllers/results_controller.rb
@@ -1,6 +1,8 @@
 class ResultsController < ApplicationController
   def index
+
     @results = Result.all.order('updated_at DESC')
+    @last_run = @results.first.updated_at.localtime
     conditions = {}
     conditions[:item_id] = params[:item] unless params[:item].blank?
     conditions[:shop_id] = params[:shop] unless params[:shop].blank?
