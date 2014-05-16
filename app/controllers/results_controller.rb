eba5ebb41094af7c302f3bb03aa2569790ad34ec
inv-ftw <inv.ftw@gmail.com>
2014-05-16 17:52:59 +0300

    delete pry

--- app/controllers/results_controller.rb
+++ app/controllers/results_controller.rb
@@ -15,7 +15,7 @@ class ResultsController < ApplicationController
   end
 
   def proceed
-    binding.pry
+    #binding.pry
     options = params
     options[:r_item] = params[:r_item].join(',') if params[:r_item].present?
     call_rake(:get_results, options)
